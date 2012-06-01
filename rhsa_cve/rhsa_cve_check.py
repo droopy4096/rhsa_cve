42c15eb6a536a10ec4727aaa04b3fd22059a6bc9
Dmitry Makovey <dmakovey@yahoo.com>
2012-06-01 14:16:36 -0600

    Implemented rudimentary parsers for CVE and CPE

--- rhsa_cve/rhsa_cve_check.py
+++ rhsa_cve/rhsa_cve_check.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+from __future__ import print_function
+
 import sys
 import os, os.path
 import urllib2
@@ -8,6 +10,13 @@ import datetime
 import argparse
 import gzip
 import csv
+import re
+
+from UserDict import UserDict
+
+from xml.dom.minidom import parse as minidom_parse
+# from xml.etree.ElementTree import ElementTree
+
                                                                                                                      
 ## Based on information at:                                                                                          
 ##  https://www.redhat.com/security/data/metrics/                                                                    
@@ -20,7 +29,8 @@ CPE_DICT_URL='https://www.redhat.com/security/data/metrics/cpe-dictionary.xml'
 def fetch(url,filename,force=False):
     if not os.path.exists(filename) or force:
         # fetch CVE stuff
-        remote=urllib2.urlopen(CVE_ALLITEMS_GZ_URL)
+        print("Fetching {0} from {1}".format(filename,url))
+        remote=urllib2.urlopen(url)
         local=open(filename,'w')
         can=True
         while can:
@@ -32,12 +42,90 @@ def fetch(url,filename,force=False):
         remote.close()
         local.close()
 
+def isEmpty(my_list):
+    for i in my_list:
+        if i:
+            return False
+    return True
+
 class MissingArguments(object):
     def __init__(self,text):
         self.text=text
     def __str__(self):
         return self.text
 
+class CVEList(UserDict):
+    _filter=None
+    def __init__(self,filename=None):
+        UserDict.__init__(self)
+        if filename:
+            self.load(filename)
+            
+        self._filter=None
+        self._ref_re=re.compile(r' +\| +')
+
+    def setFilter(self,filter):
+        """List only items that match filter"""
+        self._filter=filter
+
+    def load(self,filename):
+        with open(filename,'r') as f:
+            self._load(f)
+    
+    def load_gz(self,filename):
+        with gzip.open(filename,'r') as f:
+            self._load(f)
+        
+
+    def _load(self,file):
+        # with cve_csv_gz, rhsa2cve:
+        #      .....
+        reader=csv.reader(file)
+        ## 1. read line #3  for field names
+        ## 2. read the rest of the file after empty line
+        
+        while reader.line_num<3:
+            row=reader.next()
+
+        base_dict=row
+        # print( base_dict)
+        
+        while not isEmpty(row):
+            row=reader.next()
+            
+        # now we're at empty row
+        
+        for row in reader:
+            # print(row[0])
+            cve_id=row[0]
+            if self._filter:
+                # print("filter...")
+                if not (cve_id in self._filter):
+                    continue
+            raw_dict=dict(zip(base_dict,row))
+            ref_list=self._ref_re.split(raw_dict['References'])
+            raw_dict['References']=ref_list
+            self.data[cve_id]=dict(zip(base_dict,row))
+
+class CPEDict(UserDict):
+    """CPE dictionary object"""
+    
+    def __init__(self,filename=None):
+        UserDict.__init__(self)
+        if filename:
+            self.load(filename)
+        
+    def load(self,filename):
+        # cpe_tree=ElementTree()
+        # cpe_list=cpe_tree.parse(filename)
+        cpe_list=minidom_parse(filename)
+
+        # print(cpe_list)
+        for ci in cpe_list.getElementsByTagName('cpe-item'):
+            # print('found item')
+            ci_title_elem=ci.getElementsByTagName('title')[0]
+            self.data[ci.getAttribute("name")]=ci_title_elem.firstChild.nodeValue
+            
 class Analizer(object):
     cve_csv_filename=None
     cve_csv_file=None
@@ -84,25 +172,35 @@ class Analizer(object):
         ##XXX now we need to open this XML and extract dictionary of 
         ##XXX CPE descriptors
 
-
 def main(argv):
     parser = argparse.ArgumentParser(description='RHSA & CVE cross-reference tool')
     parser.add_argument('cve_candidates',type=str,help="name of the file listing CVE's",
-                        metavar='<cve_list_file>')
-
-    args = parser.parse_args()
+                         metavar='<cve_list_file>', default=None)
+     
+    args = parser.parse_args(argv[1:])
 
     today=datetime.date.today()
     today_str=today.strftime('%Y-%m-%d')
     cve_csv_filename='cve-allitems-'+today_str+'.csv'
-    cve_csv_gz_filename='cve-allitems-'+today_str+'.csv'
+    cve_csv_gz_filename='cve-allitems-'+today_str+'.csv.gz'
     rhsa2cve_filename='rhsamapcpe-'+today_str+'.txt'
     cpe_dict_filename='cpe-dictionary.xml'
 
-    cve_candidates=args.cve_candidates
+    if args.cve_candidates:
+        cve_candidates=args.cve_candidates
+    else:
+        cve_candidates='need_to_fix'
 
-    failed_csv_filename='compiled/failed-'+today+'.csv'
-    fixed_list_filename='compiled/fixed-'+today+'.txt'
+    f=open(cve_candidates,'r')
+    a=f.read()
+    f.close()
+        
+    cve_filter=a.split()
+    # print(a)
+    # print(cve_filter)
+
+    failed_csv_filename='compiled/failed-'+today_str+'.csv'
+    fixed_list_filename='compiled/fixed-'+today_str+'.txt'
 
     try:
         os.mkdir('compiled')
@@ -113,19 +211,24 @@ def main(argv):
     fetch(CVE_ALLITEMS_GZ_URL,cve_csv_gz_filename)
     fetch(RHSA_MAP_CPE_TXT_URL,rhsa2cve_filename)
     fetch(CPE_DICT_URL,cpe_dict_filename)
+    
+    cpe=CPEDict(cpe_dict_filename)
+    cve=CVEList()
+    cve.setFilter(cve_filter)
+    cve.load_gz(cve_csv_gz_filename)
+    print(cve)
+    return
 
-    cve_csv_gz=gzip.open(cve_csv_gz_filename,'r')
     rhsa2cve=open(rhsa2cve_filename,'r')
     # with cve_csv_gz, rhsa2cve:
     #      .....
 
-    cve_reader=csv.reader(cve_csv_gz)
-    for row in cve_reader:
-        print(','.join(row))
 
-    cve_csv_gz.close()
     rhsa2cve.close()
 
+
+if __name__ == '__main__':
+    main(sys.argv)
 
 
 ##### SHELL ######

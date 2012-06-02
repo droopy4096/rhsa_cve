5693ecb21ee2009526df9c828043461ff2bfb834
droopy4096 <dmakovey@yahoo.com>
2012-06-01 21:13:06 -0600

    finally on-par with bash version

--- rhsa_cve/rhsa_cve_check.py
+++ rhsa_cve/rhsa_cve_check.py
@@ -203,8 +203,173 @@ class CPEDict(UserDict):
             self.data[ci.getAttribute("name")]=ci_title_elem.firstChild.nodeValue
             
 class CveRhsaAnalyzer(object):
-    def __init__(self):
-        pass
+
+    _cve_dict=None
+    _cpe_dict=None
+    _rhsa2cve_dict=None
+    
+    def __init__(self,cve,rhsa2cve,cpe):
+        self.setCpeDict(cpe)
+        self.setCveDict(cve)
+        self.setRhsa2CveDict(rhsa2cve)
+
+    def setCveDict(self,cve):
+        self._cve_dict=cve
+        
+    def setCpeDict(self,cpe):
+        self._cpe_dict=cpe
+        
+    def setRhsa2CveDict(self,rhsa2cve):
+        self._rhsa2cve_dict=rhsa2cve
+    
+    def get_cve_compliance_report(self):
+        """Map out CVE->RHSA with attached package names affected
+        Returns tuple: (CVE-ID:str,Fixed:bool,RHSA:list,pkgs:set)"""
+        #TODO add ability to generate package->CVE list for automated checking  
+        cve=self._cve_dict
+        cpe=self._cpe_dict
+        rhsa2cve=self._rhsa2cve_dict
+        
+        rev_map={}
+        for rhsa in rhsa2cve.keys():
+            cve_list=rhsa2cve[rhsa]['CVE']
+            for c in cve_list:
+                if rev_map.has_key(c):
+                    rev_map[c].append(rhsa)
+                else:
+                    rev_map[c]=[rhsa]
+            # print(rhsa, ",".join(cve_list))
+        report=[]
+        for cve_name in cve.keys():
+            if rev_map.has_key(cve_name):
+                rev_lookup=rev_map[cve_name]
+                # print(rev_lookup)
+                pkg_set=set()
+                for r in rev_lookup:
+                    cpe_list=rhsa2cve[r]['CPE']
+                    for cpe_item in cpe_list:
+                        pkg_set.add(cpe_item['package'])
+                # print(cve_name,",".join(rev_lookup),",".join(pkg_list))
+                report.append((cve_name,True,rev_lookup,pkg_set))
+            else:
+                # print(cve_name,"NOT FIXED")
+                report.append((cve_name,False,(),()))
+        return report
+        
+    def get_package_cve_map(self,cve_report=None):
+        """create a list of packages with CVE items to check"""
+        # we need something we can use for meta-loop:
+        # for p in packages:
+        #   login to server
+        #   rpm --changelog -q $p | grep $cve 
+        cve=self._cve_dict
+        cpe=self._cpe_dict
+        rhsa2cve=self._rhsa2cve_dict
+        pkg_dict={}
+        if not cve_report:
+            cve_report=self.get_cve_compliance_report()
+        for (cve_id,status,rhsa,pkg_set) in cve_report:
+            if status:
+                for p in pkg_set:
+                    if not pkg_dict.has_key(p):
+                        pkg_dict[p]=[]
+                    pkg_dict[p].append(cve_id)
+        return pkg_dict
+
+class CheckApplication(object):
+
+    _cve_candidates_filename=None
+    _cve_dict=None
+    _cpe_dict=None
+    _rhsa2cve_dict=None
+
+    _failed_csv_filename=None
+    _fixed_list_filename=None
+
+    def createParser(self):
+        parser = argparse.ArgumentParser(description='RHSA & CVE cross-reference tool')
+        parser.add_argument('cve_candidates',type=str,help="name of the file listing CVE's",
+                             metavar='<cve_list_file>', default=None)
+        return parser
+
+    def setupFiles(self):
+        today=datetime.date.today()
+        today_str=today.strftime('%Y-%m-%d')
+        cve_csv_filename='cve-allitems-'+today_str+'.csv'
+        cve_csv_gz_filename='cve-allitems-'+today_str+'.csv.gz'
+        rhsa2cve_filename='rhsamapcpe-'+today_str+'.txt'
+        cpe_dict_filename='cpe-dictionary.xml'
+        
+        # print(a)
+        # print(cve_filter)
+    
+        self._failed_csv_filename='compiled/failed-'+today_str+'.csv'
+        self._fixed_list_filename='compiled/fixed-'+today_str+'.txt'
+    
+        try:
+            os.mkdir('compiled')
+        except OSError:
+            # directory exists, it's fine
+            pass
+    
+        fetch(CVE_ALLITEMS_GZ_URL,cve_csv_gz_filename)
+        fetch(RHSA_MAP_CPE_TXT_URL,rhsa2cve_filename)
+        fetch(CPE_DICT_URL,cpe_dict_filename)
+        return (cve_csv_gz_filename,rhsa2cve_filename,cpe_dict_filename)
+        
+    def __init__(self,argv):
+         
+        parser=self.createParser()
+        args = parser.parse_args(argv[1:])
+
+        if args.cve_candidates:
+            self._cve_candidates_filename=args.cve_candidates
+        else:
+            self._cve_candidates_filename='need_to_fix'
+            
+        (cve_csv_gz_filename,rhsa2cve_filename,cpe_dict_filename)=self.setupFiles()
+
+        with open(self._cve_candidates_filename,'r') as f:
+            cve_list_str=f.read()
+        
+        # all we have to do is split giant string
+        # into CVE items:
+        cve_filter=cve_list_str.split()
+
+        cpe=CPEDict(cpe_dict_filename)
+        cve=CVEList()
+        cve.setLoadFilter(cve_filter)
+        cve.load_gz(cve_csv_gz_filename)
+        rhsa=Rhsa2CveMap()
+        rhsa.setLoadFilter(cve_filter)
+        # rhsa.setLoadCPEFilter(['cpe:/o:redhat:enterprise_linux'])
+        rhsa.load(rhsa2cve_filename)
+        
+        self._cpe_dict=cpe
+        self._cve_dict=cve
+        self._rhsa2cve_dict=rhsa
+
+        
+    def printCveReport(self,cra=None,cve_report=None):
+        (cve,rhsa,cpe)=(self._cve_dict,self._rhsa2cve_dict,self._cpe_dict)
+
+        if cra:
+            cr=cra
+        else:
+            cr=CveRhsaAnalyzer(cve,rhsa,cpe)
+        
+        if cve_report:
+            report=cve_report
+        else:
+            report=cr.get_cve_compliance_report()
+        for (cve_id,status,rhsa_list,pkg_list) in report:
+            if status:
+                print(cve_id,",".join(rhsa_list),",".join(pkg_list))
+            else:
+                print(cve_id,'NOT FIXED')
+                
+        pkg_cve=cr.get_package_cve_map(report)
+        print(pkg_cve)
 
 ##### SHELL ######
 #===============================================================================
@@ -224,105 +389,35 @@ class CveRhsaAnalyzer(object):
 #   fi
 #  done 
 #===============================================================================
-    
-    def get_cve_compliance_report(self,cve=None,cpe=None,rhsa2cve=None):
-        """Currently map out CVE->RHSA with attached package names affected
-        Returns tuple: (CVE-ID:str,Fixed:bool,RHSA:list,pkg_list:list)"""
-        #TODO add ability to generate package->CVE list for automated checking  
-        rev_map={}
-        for rhsa in rhsa2cve.keys():
-            cve_list=rhsa2cve[rhsa]['CVE']
-            for c in cve_list:
-                if rev_map.has_key(c):
-                    rev_map[c].append(rhsa)
-                else:
-                    rev_map[c]=[rhsa]
-            # print(rhsa, ",".join(cve_list))
-        report=[]
-        for cve_name in cve.keys():
-            if rev_map.has_key(cve_name):
-                rev_lookup=rev_map[cve_name]
-                # print(rev_lookup)
-                pkg_list=set()
-                for r in rev_lookup:
-                    cpe_list=rhsa2cve[r]['CPE']
-                    for cpe_item in cpe_list:
-                        pkg_list.add(cpe_item['package'])
-                # print(cve_name,",".join(rev_lookup),",".join(pkg_list))
-                report.append((cve_name,True,rev_lookup,pkg_list))
-            else:
-                # print(cve_name,"NOT FIXED")
-                report.append((cve_name,False,(),()))
-        return report
         
-    def package_cve_map(self,cve,cpe,rhsa2cve):
-        """create a list of packages with CVE items to check"""
-        # we need something we can use for meta-loop:
-        # for p in packages:
-        #   login to server
-        #   rpm --changelog -q $p | grep $cve 
-        raise "NotImplemented"
+    def createCveReportFiles(self,cra=None,cve_report=None):
+        (cve,rhsa,cpe)=(self._cve_dict,self._rhsa2cve_dict,self._cpe_dict)
 
-
-def main(argv):
-    parser = argparse.ArgumentParser(description='RHSA & CVE cross-reference tool')
-    parser.add_argument('cve_candidates',type=str,help="name of the file listing CVE's",
-                         metavar='<cve_list_file>', default=None)
-     
-    args = parser.parse_args(argv[1:])
-
-    today=datetime.date.today()
-    today_str=today.strftime('%Y-%m-%d')
-    cve_csv_filename='cve-allitems-'+today_str+'.csv'
-    cve_csv_gz_filename='cve-allitems-'+today_str+'.csv.gz'
-    rhsa2cve_filename='rhsamapcpe-'+today_str+'.txt'
-    cpe_dict_filename='cpe-dictionary.xml'
-
-    if args.cve_candidates:
-        cve_candidates=args.cve_candidates
-    else:
-        cve_candidates='need_to_fix'
-
-    f=open(cve_candidates,'r')
-    a=f.read()
-    f.close()
-        
-    cve_filter=a.split()
-    # print(a)
-    # print(cve_filter)
-
-    failed_csv_filename='compiled/failed-'+today_str+'.csv'
-    fixed_list_filename='compiled/fixed-'+today_str+'.txt'
-
-    try:
-        os.mkdir('compiled')
-    except OSError:
-        # directory exists, it's fine
-        pass
-
-    fetch(CVE_ALLITEMS_GZ_URL,cve_csv_gz_filename)
-    fetch(RHSA_MAP_CPE_TXT_URL,rhsa2cve_filename)
-    fetch(CPE_DICT_URL,cpe_dict_filename)
-    
-    cpe=CPEDict(cpe_dict_filename)
-    cve=CVEList()
-    cve.setLoadFilter(cve_filter)
-    cve.load_gz(cve_csv_gz_filename)
-    rhsa=Rhsa2CveMap()
-    rhsa.setLoadFilter(cve_filter)
-    # rhsa.setLoadCPEFilter(['cpe:/o:redhat:enterprise_linux'])
-    rhsa.load(rhsa2cve_filename)
-    
-    cr=CveRhsaAnalyzer()
-    report=cr.get_cve_compliance_report(cve, cpe, rhsa)
-    for (cve_id,status,rhsa_list,pkg_list) in report:
-        if status:
-            print(cve_id,",".join(rhsa_list),",".join(pkg_list))
+        if cra:
+            cr=cra
         else:
-            print(cve_id,'NOT FIXED')
+            cr=CveRhsaAnalyzer(cve,rhsa,cpe)
+        
+        if cve_report:
+            report=cve_report
+        else:
+            report=cr.get_cve_compliance_report()
             
-    cr.package_cve_map(cve, cpe, rhsa)
+        writer=csv.writer(open(self._failed_csv_filename, 'wb'),quoting=csv.QUOTE_NONNUMERIC)
+        writer.writerow(["Name","Status","Description","Phase","Comments"])
+        for (cve_id,status,rhsa_list,pkg_list) in report:
+            if not status:
+                cve_item=cve[cve_id]
+                writer.writerow((cve_id,cve_item["Status"],cve_item["Description"],cve_item["Phase"],cve_item["Comments"]))
+        
+        with open(self._fixed_list_filename,'w') as f:
+            for (cve_id,status,rhsa_list,pkg_list) in report:
+                if status:
+                    print(cve_id,",".join(rhsa_list),",".join(pkg_list),file=f)
+        
 
 if __name__ == '__main__':
-    main(sys.argv)
+    ca=CheckApplication(sys.argv)
+    ca.printCveReport()
+    ca.createCveReportFiles()
 
